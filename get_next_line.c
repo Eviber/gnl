32291460a55f351febdd485ca583b36bae3803d0
Youva GAUDE <ygaude@student.42.fr>
2017-01-13 21:30:56 +0100

    Fix double newline issue

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 12:52:57 by ygaude            #+#    #+#             */
-/*   Updated: 2017/01/08 23:59:27 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/01/13 21:29:28 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,28 @@
 #include <unistd.h>
 #include "get_next_line.h"
 #include "libft/libft.h"
+
+void	delbuf(t_list **lst, const int fd)
+{
+	t_list	*cur;
+	t_list	*tmp;
+
+	cur = *lst;
+	tmp = NULL;
+	if (!*lst)
+		return ;
+	while (cur && fd != (int)cur->content_size)
+	{
+		tmp = cur;
+		cur = cur->next;
+	}
+	if (tmp)
+	{
+		tmp->next = (cur ? cur->next : NULL);
+	}
+	else
+		*lst = (*lst)->next;
+}
 
 void	ft_strappend(char **s1, char *s2)
 {
@@ -43,25 +65,33 @@ int		read_next_line(const int fd, char **line, char *buf)
 {
 	ssize_t		len;
 	char		*tmp;
-	int			check;
 
-	check = 0;
-	tmp = ft_strchr(buf, '\n');
 	len = 1;
-	ft_strappend(line, buf);
-	while (!tmp && len && len != -1)
+	if (*buf == '\n')
 	{
-		len = read(fd, buf, BUFF_SIZE);
-		if (len)
-			check = 1;
+		ft_memmove(buf, buf + 1, ft_strlen(buf));
+		return (1);
+	}
+	ft_strappend(line, buf);
+	while ((len = read(fd, buf, BUFF_SIZE)))
+	{
+		if (len == -1)
+			return (-1);
+		buf[len] = '\0';
 		tmp = ft_strchr(buf, '\n');
 		ft_strappend(line, buf);
-		buf[len] = '\0';
+		if (tmp)
+			break ;
 	}
-	if (len == -1 || (!len && !check) || !buf)
-		return ((!buf) ? -1 : len);
-	ft_memmove(buf, tmp + 1, ft_strlen(tmp));
-	tmp = ft_strchr(*line, '\n');
+	if (!ft_strlen(*line) && !len)
+		return (0);
+	if (!tmp)
+		tmp = buf;
+	else
+	{
+		ft_memmove(buf, tmp + 1, ft_strlen(tmp));
+		tmp = ft_strchr(*line, '\n');
+	}
 	*tmp = '\0';
 	return (1);
 }
@@ -75,7 +105,7 @@ int		get_next_line(const int fd, char **line)
 
 	if (fd < 0 || !line)
 		return (-1);
-	*line = ft_strnew(1);
+	*line = ft_strnew(0);
 	tmp = get_buf(fd, list);
 	buf = tmp ? (char *)tmp->content : NULL;
 	if (!buf)
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 15:16:22 by ygaude            #+#    #+#             */
-/*   Updated: 2017/01/08 21:31:06 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/01/11 17:20:17 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1337
+# define BUFF_SIZE 33
 
 int		get_next_line(const int fd, char **line);
 
