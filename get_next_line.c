9ec2f107a11b94847fed1575bdd11ea968877a39
Eviber <youv.didj@gmail.com>
2017-06-02 08:38:16 +0200

    OMG THAT FUCKING BUG WAS THERE

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/15 14:16:47 by ygaude            #+#    #+#             */
-/*   Updated: 2017/06/02 05:55:52 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/06/02 08:36:53 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include "get_next_line.h"
 #include "libft/libft.h"
-/*
+
 static char	*getbuf(const int fd)
 {
 	static t_list	*catalog = NULL;
@@ -27,48 +27,47 @@ static char	*getbuf(const int fd)
 		if (!(catalog = (t_list *)ft_memalloc(sizeof(t_list))))
 			return (NULL);
 	cur = catalog;
-	while ((int)cur->content_size - 1 != fd && cur->next)
+	while ((int)cur->content_size != fd && cur->next)
 		cur = cur->next;
-	if ((int)cur->content_size - 1 != fd)
+	if ((int)cur->content_size != fd)
 	{
 		if (!(res = ft_strnew(BUFF_SIZE)))
 			return (NULL);
-		ft_lstaddend(&catalog, ft_lstnew((const void *)res, (size_t)(fd + 1)));
+		ft_lstaddend(&catalog, ft_lstnew((const void *)res, BUFF_SIZE + 1));
 		ft_strdel(&res);
 		cur = cur->next;
+		cur->content_size = (size_t)fd;
 	}
 	res = (char *)cur->content;
 	return (res);
 }
-*/
+
 int			get_next_line(const int fd, char **line)
 {
-	static char				*buf;
+	char				*buf;
 	char				*tmp;
 	ssize_t				ret;
 
-	ft_putstr("\033[0;31m");
-	ft_putendl(buf);
-	ft_putstr("\033[0m");
 	ret = 1;
-	buf = ft_strnew(BUFF_SIZE); //getbuf(fd);
+	buf = getbuf(fd);
 	if (!buf || !line || fd < 0 || read(fd, buf, 0) < 0)
 		return (-1);
 	*line = NULL;
 	tmp = ft_strdup(buf);
-	while (!ft_strchr(tmp, '\n') && (ret = read(fd, buf, BUFF_SIZE)) > 0)
+	while (tmp && !ft_strchr(tmp, '\n') && (ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		if(!(tmp = ft_strappend(&tmp, &buf, 'F')))
-			return (-1);
+		tmp = ft_strappend(&tmp, &buf, 'F');
 	}
-	if (ret == -1)
+	if (!tmp || ret == -1)
 		return (-1);
-	*line = ft_strsub(tmp, 0, (ft_strchr(tmp, '\n')) ?
-		((size_t)(ft_strchr(tmp, '\n') - tmp)) : ft_strlen(tmp));
+	if (!(*line = ft_strsub(tmp, 0, (ft_strchr(tmp, '\n')) ?
+		((size_t)(ft_strchr(tmp, '\n') - tmp)) : ft_strlen(tmp))))
+		return (-1);
 	ft_strclr(buf);
 	if (ft_strchr(tmp, '\n'))
 		ft_strcpy(buf, (ft_strchr(tmp, '\n') + 1));
-	ft_strdel(&tmp);
+	free(tmp);
+	tmp = NULL;
 	return ((ret || ft_strlen(buf) || ft_strlen(*line)) ? 1 : 0);
 }
